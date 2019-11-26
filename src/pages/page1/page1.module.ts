9dcf7c6f73442930de93048c9bd412dd5990a019
marcos <marcos@localhost.localdomain>
2019-11-26 16:18:37 -0300

    exercicio 2

--- src/pages/page1/page1.module.ts
+++ src/pages/page1/page1.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { Page1 } from './page1';
+import { Page2 } from '../page2/page2';
 
 @NgModule({
   declarations: [
-    Page1,
+    Page1
   ],
   imports: [
     IonicPageModule.forChild(Page1),
--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { Page2 } from '../page2/page2';
 
 @IonicPage()
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
+
 export class Page1 {
-
+  public cumprimento;
+  public nome;
   constructor(public navCtrl: NavController) {
-
+    this.nome = "Fulano";
+    this.cumprimento = "Oi, ";
+  }
+  trocar(){
+    this.navCtrl.push("Page2",{
+      nome: this.nome,
+      calback : this.calback,
+      that :this
+    });
   }
 
+
+
+  calback(retorno,that){
+    that.nome = retorno;
+    that.cumprimento = "Oi"+(retorno!=""?", ":"");
+  }
 }
